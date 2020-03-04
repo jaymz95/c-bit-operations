bf72d5803f47bd418edf677cbf90273510cac92b
jaymz95 <jamesmullarkey15@gmail.com>
2020-03-04 16:24:55 +0000

    orperators implemented

--- lookatbits.c
+++ lookatbits.c
@@ -1,13 +1,36 @@
 #include <stdio.h>
+#include <inttypes.h>
 
 int main(int argc, char *argv[]){
-	char c = 65;
-	int i = 65;
+	
+  uint8_t a = 73;
+  uint8_t b = 99;
+  uint8_t c = (a ^ b);
 
-	printf("%c\t%d\t%d\n", c, c, sizeof(c));
-	printf("%c\t%d\t%d\n", i, i, sizeof(i));
+  printf("a = "); printbits(a); printf("\n");
+  printf("b = "); printbits(b); printf("\n");
+  printf("c = "); printbits(c); printf("\n");
 
+  printf("\n\n");
 
-	return 0;
+  printf(" a = "); printbits( a); printf("\n");
+  printf("~a = "); printbits(~a); printf("\n");
 
+  printf("\n\n");
+
+  printf(" 1       = "); printbits( 1      ); printf("\n");
+  printf("(1 << 3) = "); printbits((1 << 3)); printf("\n"); 
+
+  printf("\n\n");
+
+  printf(" 99       = "); printbits(            99                   ); printf("\n");
+  printf("(99 << 3) = "); printbits((((uint8_t) 99) << ((uint8_t) 3))); printf("\n"); 
+
+  printf("\n\n");
+
+  printf("              99 = "); printbits(              99); printf("\n");
+  printf("(1ULL << 5)      = "); printbits((1ULL << 5)     ); printf("\n");
+  printf("(1ULL << 5) & 99 = "); printbits((1ULL << 5) & 99); printf("\n"); 
+
+  return 0;
 }
